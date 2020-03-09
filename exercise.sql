2905d6d14deb3307ddf561b51954a5cec23fe62f
Justin Plein <2099449@talnet.nl>
2020-03-09 11:40:01 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,14 +1,50 @@
 
 SELECT 
 title,
-rating,
-description,
-has_won_awards,
-seasons,
+has_won_awards
+FROM series
+WHERE has_won_awards = 1;
+
+SELECT
+title,
+rating
+FROM series
+WHERE rating > 2.5;
+
+SELECT
+title,
 country,
 language
+FROM series
+WHERE country = 'NL' AND language = 'NL';
+
+SELECT
+title,
+seasons
+FROM series
+WHERE seasons < 5;
+
+SELECT 
+MAX(seasons) 
+AS rating
 FROM series;
- 
- SELECT * FROM series;
- 
-USE `netland`;
+
+SELECT
+title,
+seasons
+FROM series
+WHERE seasons < 3 OR seasons > 20;
+
+SELECT 
+title
+FROM series
+WHERE title LIKE "%th%";
+
+SELECT
+title,
+seasons
+FROM series
+WHERE seasons != 3;
+
+SELECT * FROM series;
+
